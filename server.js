7c2707b51c98895d8638ee64f8a449caa8d13087
Eric P. Katz <ericpkatz@gmail.com>
2019-04-11 15:11:23 -0400

    commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const { sync  } = require('./db');
+const axios = require('axios');
 
 app.listen(process.env.PORT || 3000);
 
@@ -8,3 +9,18 @@ if(SYNC){
   sync[SYNC]()
     .then(()=> console.log(`sync ${SYNC} done`));
 }
+
+const ping = ()=> {
+  return axios.get("https://acme-users-api.herokuapp.com/api/users")
+    .then(()=> console.log('good to go'));
+};
+
+
+const delay = ()=> {
+  setTimeout(function() {
+    ping()
+      .then( ()=> delay());
+  }, process.env.DELAY*1); // every 5 minutes (300000)
+}
+
+delay();

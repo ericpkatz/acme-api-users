6d82343a7a5c2d2e86abe708e83d2bd96d0e677f
Eric P. Katz <ericpkatz@gmail.com>
2019-08-24 07:45:46 -0400

    remove rate limiter

--- app.js
+++ app.js
@@ -10,17 +10,19 @@ const path = require('path');
 app.use(cors());
 const Op = require('sequelize').Op;
 
+/*
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
 });
+app.use(limiter);
+*/
 
 
 module.exports = app;
 
-app.use(limiter);
 
 
 const PAGE_SIZE = process.env.PAGE_SIZE || 50;
